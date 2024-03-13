5e7fd3f3a9ec30ce55eb9d4712293fd7d5539198
Gosyy <114762139+Gosyy@users.noreply.github.com>
2024-03-13 17:54:06 +0500

    Added Tasl_3 in Seminar_1 folder

---
+++ Seminar_1/Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// На вход поступает трёхзначное число.
+// Вывести сумму первой и последней цифр числа.
+
+System.Console.Write("Введите целое трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (bool i = false; i != true;)
+{
+    if (n / 100 > 0 && n / 100 < 10) // Проверка на трёхзначность
+    {
+        i = true;
+    }
+
+    else 
+    {
+        System.Console.Write("Введите число, согласно условию - целое трёхзначное: ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+System.Console.Write($"{n} => {n / 100} + {n % 10} = {n / 100 + n % 10} \n");
