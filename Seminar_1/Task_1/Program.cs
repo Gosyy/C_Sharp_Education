a52e9f9c9772a65dd4fe302604c461de21b354b2
Gosyy <114762139+Gosyy@users.noreply.github.com>
2024-03-13 16:29:55 +0500

    Added Task_2

---
+++ Seminar_1/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// На вход поступают два целых числа. 
+// Необходимо проверить, является-ли одно квадратом другого.
+
+Console.Write ("Введите первое число: ");
+int a = Convert.ToInt32 (Console.ReadLine()); 
+
+// ReadLine принимает значение в формате char
+// Convert позволяет конвертировать char в любой тип
+
+Console.Write ("Введите второе число: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+
+if (b * b == a) 
+{
+    // System.Console.Write ("a = " + a + ", " + "b = " + b + " => Да \n");
+    // Знак + (кокретинации) для связи текста и переменных при выводе
+
+    System.Console.Write ($"a = {a}, b = {b} => Да");
+    // $" ... {x} ... " - записать переменные и текст
+}
+
+else
+{
+    // System.Console.Write ("a = " + a + ", " + "b = " + b + " => Нет \n");
+    System.Console.Write ($"a = {a}, b = {b} => Нет");
+}
