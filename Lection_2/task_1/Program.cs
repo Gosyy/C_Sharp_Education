45c5ed64c0268b5f5c08b752fa5b2364f5ce453a
Gosyy <114762139+Gosyy@users.noreply.github.com>
2024-03-22 13:45:47 +0500

    Added task_1 massive

---
+++ Lection_2/task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Массивы
+
+int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 1;
+arr[2] = 8;
+arr[3] = 3;
+arr[4] = 0;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr_2 = new int[5] { 1, 2, 4, 5, 7 };
+
+int[] arr_3 = { 2, 4, 7, 1, 8 };
