126119149e0f2c2609539991bbba4bcc271a0420
Gosyy <114762139+Gosyy@users.noreply.github.com>
2024-03-22 15:35:12 +0500

    Added task 1 to seminar 2/homework folder

---
+++ Lection_2/task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Создать рандомный массив и вывести ЧЕТНЫЕ числа.
+
+using System; // Для Randor
+
+System.Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Random rand = new Random();
+
+int[] arr = new int[n];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next (-10, 10);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
---
+++ Seminar_2/homework/task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Напишите программу, 
+// которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] multiplicity = [7, 23]; // multiplicity - анг. кратность
+
+if (number % multiplicity[0] == 0 && number % multiplicity[1] == 0)
+{
+    System.Console.WriteLine($"number = {number} => кратно {multiplicity[0]} и {multiplicity[1]}");
+}
+else System.Console.WriteLine($"number = {number} =>  не кратно {multiplicity[0]} и {multiplicity[1]}");
