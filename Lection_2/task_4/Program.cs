0995ce65d90a8c2b276841bfae60ec6d0a044e9f
Gosyy <114762139+Gosyy@users.noreply.github.com>
2024-03-22 14:31:11 +0500

    Added task 4 maximal element of massive

--- Lection_2/task_4/Program.cs
+++ Lection_2/task_4/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Создать рандомный массив, вывести его, а также максимальное число.
+
+using System; // Для Randor
+
+System.Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Random rand = new Random();
+
+int[] arr = new int[n];
+
+for (int i = 0; i < arr.Length; i++) // Заполнение массива числами
+{
+    arr[i] = rand.Next (-10, 10);
+}
+
+for (int i = 0; i < arr.Length; i++) // Вывод элементов массива
+{
+    Console.Write($"{arr[i]} ");
+}
+
+int max = arr[0];
+
+for (int i = 1; i < arr.Length; i++) // Поиск максимального числа
+{
+    if (max > arr[i])
+    {
+        max = arr[i];
+    }
+}
+
+Console.Write($"\n Максимальный элемент массива: {max} \n");
