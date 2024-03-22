1e29d5f2267e1ebaa565d32c98f1004894c426e0
Gosyy <114762139+Gosyy@users.noreply.github.com>
2024-03-22 14:13:50 +0500

    Added Task_2 output random massive

---
+++ Lection_2/task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Создать рандомный массив и вывести его на экран.
+
+using System; // Для Randor
+
+System.Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Random rand = new Random();
+
+int[] arr = new int[n];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next (-10, 10);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
